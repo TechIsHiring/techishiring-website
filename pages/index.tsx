4aa032ce9358468beb4b3bc8f4f8b72550d56046
chadstewart <chadrhonanstewart@gmail.com>
2022-10-14 18:06:22 -0500

    Fixing an issue with the vercel logo link.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import vercelLogo from "public/vercel.svg";
+import vercelLogo from "../public/vercel.svg";
 
 const Home: NextPage = () => {
   return (
