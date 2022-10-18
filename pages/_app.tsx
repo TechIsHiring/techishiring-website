2ccb331fc230f678e5940760e795bb3922e6b2f0
chadstewart <chadrhonanstewart@gmail.com>
2022-10-18 09:07:24 -0500

    Set up layouts in _app.tsx. Also updated next from latest to ^12.3.1 to avoid breaking changes on deployment.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+type ComponentWithPageLayout = AppProps & {
+  Component: AppProps["Component"] & {
+    PageLayout?: React.ComponentType<any>;
+  };
+};
+
+function MyApp({ Component, pageProps }: ComponentWithPageLayout) {
+  return (
+    <>
+      {Component.PageLayout ? (
+        <Component.PageLayout>
+          <Component {...pageProps} />
+        </Component.PageLayout>
+      ) : (
+        <Component {...pageProps} />
+      )}
+    </>
+  );
 }
 
 export default MyApp
