e84a90d7c2bf05307d8bd22802c75a270accdbc3
chadstewart <chadrhonanstewart@gmail.com>
2022-10-19 10:49:15 -0500

    Forgot to add a story for default layout so quickly threw one together.

---
+++ src/stories/templates/layouts/home.stories.tsx
@@ -0,0 +1,11 @@
+import DefaultLayout from "../../../components/templates/layouts/default-layout";
+
+const storyConfig = {
+  title: "Design System/Templates/Layouts/Default Layout"
+};
+
+export default storyConfig;
+
+export const DefaultLayoutStory = () => <DefaultLayout>
+                                  Hello
+                               </DefaultLayout>;
