de951d5f4c2779661af7e27d21701f503903a47c
chadstewart <chadrhonanstewart@gmail.com>
2022-10-21 14:53:22 -0500

    Started to do initial seo stuff.

--- src/components/templates/layouts/default-layout.tsx
+++ src/components/templates/layouts/default-layout.tsx
@@ -1,17 +1,24 @@
-import DefaultAppHead from 'components/atoms/default-app-head/default-app-head';
+import Head from 'next/head';
 import Footer from 'components/organisms/footer/footer';
 import Header from 'components/organisms/header/header';
 
 const DefaultLayout = ({children}: {children: React.ReactNode}) => {
   return (
+    <>
+    <Head>
+      <meta 
+        name="viewport"
+        content="width=device-width, initial-scale=1"
+      />
+    </Head>
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <DefaultAppHead />
       <Header />
       <main className="flex w-full min-h-[calc(100vh-190px)] flex-1 flex-col px-5 py-10 text-center">
         {children}
       </main>
       <Footer />
     </div>
+    </>
   )
 };
 
