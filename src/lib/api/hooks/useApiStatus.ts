e53f351765a6453dd1faedd63071f02da5e9d358
chadstewart <chadrhonanstewart@gmail.com>
2022-10-22 08:54:58 -0500

    Building api statues file.

---
+++ src/lib/api/hooks/useApiStatus.ts
@@ -0,0 +1,26 @@
+import { useState, useMemo } from "react";
+import { IDLE, defaultStatuses } from "../constants/api-status";
+import capitalize from "lib/utils/capitalize";
+
+const prepareStatuses = (currentStatus: string) => {
+  const statuses: { [key: string]: boolean } = {};
+
+  for (const status of defaultStatuses) {
+    const normalizedStatus = capitalize(status.toLowerCase());
+    const normalizedStatusKey = `is${normalizedStatus}`;
+    statuses[normalizedStatusKey] = status === currentStatus;
+  }
+
+  return statuses;
+};
+
+export const useApiStatus = (currentStatus = IDLE) => {
+  const [ apiStatus, setApiStatus ] = useState(currentStatus);
+  const statuses = useMemo(() => prepareStatuses(apiStatus), [apiStatus]);
+
+  return {
+    apiStatus,
+    setApiStatus,
+    ...statuses
+  };
+};
