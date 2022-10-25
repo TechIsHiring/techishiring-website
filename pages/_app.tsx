02362543a7914dcd12d15753ee1359532d121a3e
Chad Rhonan Stewart <chadrhonanstewart@gmail.com>
2022-10-25 10:09:51 -0500

    Add posthog to website! (#8)
    * changed .env variable to .env.local and added posthog id. Will add posthog id to server as well.
    
    * Successfully added posthog to app.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { initiateAnalytics } from "../src/lib/utils/analytics";
+import posthog from "posthog-js";
 
 type ComponentWithPageLayout = AppProps & {
   Component: AppProps["Component"] & {
@@ -9,6 +13,21 @@ type ComponentWithPageLayout = AppProps & {
 };
 
 function MyApp({ Component, pageProps }: ComponentWithPageLayout) {
+  
+  // From documentation on using Posthog with Next.js: https://posthog.com/docs/integrate/third-party/next-js
+  const router = useRouter();
+  
+  useEffect(() => {
+    initiateAnalytics();
+
+    const handleRouteChange = () => posthog.capture("$pageview");
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <ChakraProvider>
       {Component.PageLayout ? (
