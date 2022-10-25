79ecf1bc32ada01fc288d6b3676ff04b79701449
chadstewart <chadrhonanstewart@gmail.com>
2022-10-25 08:57:00 -0500

    Created card component and it's corresponding story.

---
+++ src/components/atoms/card/card.tsx
@@ -0,0 +1,36 @@
+interface CardCommonProps {
+  children: JSX.Element;
+  className?: string;
+}
+
+interface IsSection extends CardCommonProps {
+  section: boolean;
+  article?: never;
+}
+
+interface IsArticle extends CardCommonProps {
+  article: boolean;
+  section?: never;
+}
+
+interface NeitherArticleOrSection extends CardCommonProps {
+  article?: never;
+  section?: never;
+}
+
+type CardProps = IsArticle | IsSection | NeitherArticleOrSection;
+
+const Card = ({ children, className, section, article }: CardProps) => {
+
+  const cardClassDefinition = `${className ? className : ""} w-full p-4 shadow-md rounded-lg border-[1px] border-grey`;
+
+  return (
+    <>
+      {section && <section className={cardClassDefinition} >{children}</section>}
+      {article && <article className={cardClassDefinition} >{children}</article>}
+      {!article && !section && <div className={cardClassDefinition} >{children}</div>}
+    </>
+  );
+};
+
+export default Card;
---
+++ src/stories/atoms/card.stories.tsx
@@ -0,0 +1,11 @@
+import Card from "../../components/atoms/card/card";
+
+const storyConfig = {
+  title: "Design System/Atoms/Card"
+};
+
+export default storyConfig;
+
+export const CardStory = () => <Card>
+                                <>Hello</>
+                               </Card>;
