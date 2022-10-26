70d9a5d909742e7a98c4d90b8621096af7c6886e
chadstewart <chadrhonanstewart@gmail.com>
2022-10-26 09:46:38 -0500

    Added subscribers api (not tested yet) and beginning putting together newsletter page.

---
+++ src/stories/atoms/text-input.stories.tsx
@@ -0,0 +1,12 @@
+import TextInput from "../../components/atoms/text-input/text-input";
+
+const storyConfig = {
+  title: "Design System/Atoms/Text Input"
+};
+
+export default storyConfig;
+
+export const TextInputStory = () =>
+  <TextInput>
+    Hello
+  </TextInput>;
