7b36904d0b90c327f9a9051ca9b11bd20f503edc
chadstewart <chadrhonanstewart@gmail.com>
2022-10-27 10:00:27 -0500

    Added different states for how the email response.

--- src/components/molecules/subscribe-to-newsletter/subscribe-to-newsletter.tsx
+++ src/components/molecules/subscribe-to-newsletter/subscribe-to-newsletter.tsx
@@ -6,11 +6,14 @@ import { RouteToInternalRevueApi } from "lib/api/internal-apis/revue-subscriber/
 import { verifyIsEmail } from "lib/utils/verify-email";
 import { useState } from "react";
 
+type EmailSent = "NotSent" | "Sent" | "Error";
+
 const SubscribeToNewsletter = () => {
   const [ email, setEmail ] = useState("");
   const [ error, setError ] = useState(false);
+  const [ emailSent, setEmailSent ] = useState<EmailSent>("NotSent");
 
-  const { data, apiCall: sendEmailToRevue, statuses } = useApi(RouteToInternalRevueApi);
+  const { apiCall: sendEmailToRevue, statuses } = useApi(RouteToInternalRevueApi);
 
   const updateEmail = (textInput: string) => {
     const isEmail = verifyIsEmail(textInput);
@@ -25,27 +28,44 @@ const SubscribeToNewsletter = () => {
       const requestBody = {
         email: email
       };
-
-      await sendEmailToRevue(requestBody);
+      try {
+        await sendEmailToRevue(requestBody);
+        setEmailSent("Sent");
+      } catch (error: any) {
+        console.log(error);
+        setEmailSent("Error");
+      }
     }
-    //if(!email) setError(true);
+    if(!email) setError(true);
   };
 
   return (
     <article className="flex flex-col gap-4">
       <HeaderText level="h2">Subscribe to our newsletter!</HeaderText>
-      <div className="flex gap-2">
-        <div>
-          <TextInput
-            type="email"
-            placeholder="Enter your email"
-            onChange={event => updateEmail(event.target.value)}
-            isInvalid={error}
-          />
-          {error && <div className="p-3 text-red-700">This is not a valid email address!</div>}
+      { emailSent === "NotSent" &&
+        <div className="flex gap-2">
+          <div className="w-full lg:w-1/2">
+            <TextInput
+              type="email"
+              placeholder="Enter your email"
+              onChange={event => updateEmail(event.target.value)}
+              isInvalid={error}
+            />
+            {error && <div className="p-3 text-red-700">This is not a valid email address!</div>}
+          </div>
+          <DefaultButton onClick={handleSubscribe}>Subscribe</DefaultButton>
         </div>
-        <DefaultButton onClick={handleSubscribe}>Subscribe</DefaultButton>
-      </div>
+      }
+      { emailSent === "Sent" &&
+        <div>
+          Please check your email to confirm your subscription!
+        </div>
+      }
+      { emailSent === "Error" &&
+        <div>
+          I&apos;m sorry! Something has gone wrong with signing up your email!
+        </div>
+      }
     </article>
   );
 };
