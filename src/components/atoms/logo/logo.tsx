c49e7cce08c08090bf52e1da667f1b30e2dd3ecf
chadstewart <chadrhonanstewart@gmail.com>
2022-11-16 08:15:42 -0500

    Starting to work on accessibility issues

--- src/components/atoms/logo/logo.tsx
+++ src/components/atoms/logo/logo.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 import Link from "components/atoms/link/link";
 import TechIsHiringLogo from "../../../images/techishiring-logo.png";
 
+// Mission Start!
+
 const Logo = () => {
   return (
     <Link url="/">
