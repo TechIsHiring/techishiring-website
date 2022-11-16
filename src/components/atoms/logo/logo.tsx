74f17f105dbe66fdd438605d0d688eac22b59aa1
chadstewart <chadrhonanstewart@gmail.com>
2022-11-16 08:19:22 -0500

    Resolves #22 where the TechIsHiring logo's alt text would be difficult for screen readers to read.

--- src/components/atoms/logo/logo.tsx
+++ src/components/atoms/logo/logo.tsx
@@ -2,13 +2,11 @@ import Image from "next/image";
 import Link from "components/atoms/link/link";
 import TechIsHiringLogo from "../../../images/techishiring-logo.png";
 
-// Mission Start!
-
 const Logo = () => {
   return (
     <Link url="/">
       <a>
-        <Image className="rounded-lg" src={TechIsHiringLogo} height={70} width={254} alt="TechIsHiring logo" />
+        <Image className="rounded-lg" src={TechIsHiringLogo} height={70} width={254} alt="Tech Is Hiring logo" />
       </a>
     </Link>
   );
