e2a39dfd13268a4e4f5560f88eaebc309c2082fc
favourite1975 <godsfavour1975@gmail.com>
2023-02-14 22:54:57 +0100

    fix: fixed issue generated in the pr

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
-  images: {
-    domains: [
-      "res.cloudinary.com"
-    ]
-  }
+  reactStrictMode: true
+
 };
