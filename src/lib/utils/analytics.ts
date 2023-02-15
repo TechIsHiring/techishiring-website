fd82de5290a027bd4c5e48dfb54c6a2e0faa71a4
Roy Anger <roy@royanger.com>
2023-02-15 16:49:03 -0500

    Moved eslint-disable-next-line rule down
    This is correcting an error caused by prettier formatting.

--- src/lib/utils/analytics.ts
+++ src/lib/utils/analytics.ts
@@ -1,8 +1,8 @@
 import posthog from "posthog-js";
 
 function initiateAnalytics() {
-  // eslint-disable-next-line
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_ID as string, {
+  // eslint-disable-next-line
     api_host: "https://app.posthog.com"
   });
 }
