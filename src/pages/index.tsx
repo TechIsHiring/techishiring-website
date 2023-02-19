104705f722892b5f2021dff9871141e186a02ff3
Demehin Ibukunoluwa George <ibk2k7@gmail.com>
2023-02-19 18:49:53 +0000

    feat: completed the header component (#63)
    * feat: completed the header component
    
    * addressed merge conflicts
    
    * fix: making recommended changes
    
    * fix: adding back the accessibilit icon on main nav

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,17 @@
-
 import WithPageLayout from "interfaces/with-page-layouts";
 import DefaultLayout from "components/templates/layouts/default-layout";
 import TwitterFeed from "components/organisms/twitter-feed/twitter-feed";
 import HeadContent from "components/particles/head/HeadContent";
 import Hero from "components/molecules/heroSection/Hero";
 
-interface HomeProps {};
+interface HomeProps {}
 
 const Home: WithPageLayout<HomeProps> = () => {
   return (
     <>
-      <HeadContent content={"A space where great jobs and great devs can find each other!"}/>
+      <HeadContent
+        content={"A space where great jobs and great devs can find each other!"}
+      />
       <Hero />
       <TwitterFeed />
     </>
