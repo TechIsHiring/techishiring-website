5631a7b365b71d5024e9b0a7e233d435dda33f6c
chadstewart <chadrhonanstewart@gmail.com>
2023-04-01 17:37:15 -0500

    Removing newsletter components and respective stories.

--- src/stories/molecules/subscribe-to-newsletter.stories.tsx
+++
@@ -1,9 +0,0 @@
-import SubscribeToNewsletter from "components/molecules/subscribe-to-newsletter/subscribe-to-newsletter";
-
-const storyConfig = {
-  title: "Design System/Molecules/Subscribe to Newsletter"
-};
-
-export default storyConfig;
-
-export const SubscribeToNewsletterStory = () => <SubscribeToNewsletter />;
