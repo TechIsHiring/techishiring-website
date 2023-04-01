7a91e206558d825235db6fb09d10737b27039bae
chadstewart <chadrhonanstewart@gmail.com>
2023-04-01 08:58:31 -0500

    Added stories for the hire chad page.

---
+++ src/stories/organisms/hire-chad-content.stories.jsx
@@ -0,0 +1,9 @@
+import HireChadContent from "../../components/organisms/hire-chad-content/hire-chad-content";
+
+const storyConfig = {
+  title: "Design System/Organisms/Hire Chad Content"
+};
+
+export default storyConfig;
+
+export const HireChadContentStory = () => <HireChadContent />;
