6b209ed26d8d1548a6505df9df75ddc634525863
chadstewart <chadrhonanstewart@gmail.com>
2023-04-04 14:13:58 -0500

    Successfully sent message to backend. Just putting together email now.

--- src/lib/api/internal-apis/send-grid/index.ts
+++ src/lib/api/internal-apis/send-grid/index.ts
@@ -1,8 +1,15 @@
 import api from "../../config/api";
 import { BASEURL } from "../constants";
 
+const CONFIG: RequestInit = {
+  headers: {
+    "Content-Type": "application/json"
+  }
+};
+
 export const sendEmail = async (email: Email): Promise<any> => {
-  const response = await api.post(`${BASEURL}/api/process-email`, email);
+  const requestBody = JSON.stringify(email);
+  const response = await api.post(`${BASEURL}/api/process-email`, requestBody, CONFIG);
   const data = response.json();
   return data;
 };
--- src/pages/api/process-email.ts
+++ src/pages/api/process-email.ts
@@ -1,21 +1,26 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import sgMail from "@sendgrid/mail";
 
+interface ExtendedNextApiRequest extends NextApiRequest {
+  body: Email
+}
+
 export default async function handleSubscribe(
-  req: NextApiRequest,
+  req: ExtendedNextApiRequest,
   res: NextApiResponse<any>
-) {  
+) {
+  const { name, email, message } = req.body;
   sgMail.setApiKey(process.env.NEXT_PUBLIC_SENDGRID_API_KEY as string);
   
   const msg = {
     to: 'techishiring@gmail.com', // Change to your recipient
     from: 'techishiring@gmail.com', // Change to your verified sender
     subject: 'Sending with SendGrid is Fun',
-    text: 'and easy to do anywhere, even with Node.js',
-    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    text: message,
+    html: `<strong>${message}</strong>`,
   };
   
-  sgMail
+  /* sgMail
     .send(msg)
     .then(() => {
       console.log("Email sent");
@@ -24,5 +29,5 @@ export default async function handleSubscribe(
       return res.status(500).json({ data: error });
     });
     
-    return res.status(200).json({ data: "Email sent" });
+    return res.status(200).json({ data: "Email sent" }); */
 };
