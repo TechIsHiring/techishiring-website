88585e05ff21f17cdcb6e54f68bc25f3e0b40838
chadstewart <chadrhonanstewart@gmail.com>
2023-04-11 11:09:42 -0500

    Completed basic styling of highlights component. Just adding loading state and choosing tweets to highlight.

--- src/components/organisms/highlights-content/highlights-content.tsx
+++ src/components/organisms/highlights-content/highlights-content.tsx
@@ -1,8 +1,12 @@
+import HeaderText from "components/atoms/typography/heading-text";
 import HighlightsDetails from "components/molecules/highlights-details/highlights-details";
 
 const HighlightsContent = () => {
   return (
-    <article className="bg-[#D6E9FB] py-14">
+    <article className="flex flex-col items-center bg-[#D6E9FB] px-4 py-8">
+      <HeaderText level="h2" className="py-12">
+        TechIsHiring Highlights
+      </HeaderText>
       <HighlightsDetails />
     </article>
   );
