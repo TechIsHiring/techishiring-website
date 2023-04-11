0925d7bb32d172a268cc72393f76fa82ad488b64
chadstewart <chadrhonanstewart@gmail.com>
2023-04-11 11:52:34 -0500

    Completed loading state for tweets.

--- src/components/molecules/highlights-details/highlights-details.tsx
+++ src/components/molecules/highlights-details/highlights-details.tsx
@@ -4,9 +4,18 @@ import { TwitterTweetEmbed } from "react-twitter-embed";
 const HighlightsDetails = () => {
   return (
     <section className="flex flex-col lg:flex-row justify-center items-center gap-10">
-      <TwitterTweetEmbed tweetId="1641471955600658432" />
-      <TwitterTweetEmbed tweetId="1641471955600658432" />
-      <TwitterTweetEmbed tweetId="1641471955600658432" />
+      <TwitterTweetEmbed
+        tweetId="1641471955600658432"
+        placeholder={<Spinner />}
+      />
+      <TwitterTweetEmbed
+        tweetId="1641471955600658432"
+        placeholder={<Spinner />}
+      />
+      <TwitterTweetEmbed
+        tweetId="1641471955600658432"
+        placeholder={<Spinner />}
+      />
     </section>
   )
 };
