f11cb836d87809c7102681958040ba71d3c99f3f
Mark Noonan <mark@cypress.io>
2023-07-21 08:10:59 -0400

    test: update cypress, add component testing in CI, add Cypress Cloud (#153)
    * configure baseUrl in one place for e2e tests
    
    * update Cypress version to latest
    
    * add component testing to the github action
    
    * add cypress project id
    
    * add steps to run tests in parallel
    
    * skip building
    
    * skip building
    
    * remove extra branch, update container count

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  projectId: "1nng13",
   e2e: {
+    baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
