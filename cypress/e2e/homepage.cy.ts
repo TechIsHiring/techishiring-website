0735198ff891160f1be7006f954cbdc1168dae9a
Demian Godon <demiangodon@Demians-MacBook-Air.local>
2023-09-27 15:07:42 -0700

    refactor header tests

--- cypress/e2e/homepage.cy.ts
+++ cypress/e2e/homepage.cy.ts
@@ -1,16 +1,31 @@
+import { validateHeaderLinks, validateHeaderLogo, validateModalMenuLinks } from "../util/validateHeader";
 import validateFooter from "../util/validateFooter";
 
 describe("Homepage", () => {
   beforeEach(() => {
     cy.visit("/",  { timeout: 30000 });
+    cy.wait(100);
   });
 
   it("should display the homepage", () => {
     cy.title().should("include", "TechIsHiring");
-    cy.get("header").should("be.visible");
-    cy.get('.sticky > :nth-child(1) > a > .chakra-heading').contains('TechIsHiring');
-    cy.get('header > div > nav > ul').should('be.visible');
+    
+    validateHeaderLogo();
+    validateHeaderLinks();
 
-    validateFooter('desktop')
+    validateFooter('desktop');
   });
+
+  it("should display the homepage on mobile", () => {
+    cy.viewport('iphone-5');
+
+    cy.title().should("include", "TechIsHiring");
+    validateHeaderLogo();
+    cy.get('[data-cy=header]').within(() => {
+      cy.get('nav').click();
+    });
+    validateModalMenuLinks();
+
+    validateFooter('mobile');
+  })
 });
