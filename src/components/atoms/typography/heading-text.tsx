c21c134537bfa3e058e8d291247c262532772c78
Wayne Jones <waynejones@nyu.edu>
2023-10-07 19:56:23 +0000

    Refactored HeaderText to include props from HTMLHeadingElement

--- src/components/atoms/typography/heading-text.tsx
+++ src/components/atoms/typography/heading-text.tsx
@@ -1,14 +1,14 @@
+import React, { HTMLAttributes } from "react";
+
 type Level = Extract<keyof JSX.IntrinsicElements, 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'>;
 
-interface HeaderTextProps {
+interface HeaderTextProps extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLHeadingElement>, HTMLHeadingElement> {
   level: Level;
-  className?: string;
-  children: React.ReactNode;
 }
 
-const HeaderText = ({level: HeadingLevel, className, children}: HeaderTextProps) => {
+const HeaderText = ({level: HeadingLevel, className, children, ...props}: HeaderTextProps) => {
   return (
-    <HeadingLevel className={className}>
+    <HeadingLevel {...props} className={className}>
       {children}
     </HeadingLevel>
   );
