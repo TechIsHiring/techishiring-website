9925d4f898ab210763e69bd74bfa3f5378a79de0
Wayne Jones <waynejones@nyu.edu>
2023-10-07 18:28:47 +0000

    Refactoring DefaultButton component to include option to output as a link or button

--- src/components/atoms/button/button.tsx
+++ src/components/atoms/button/button.tsx
@@ -1,14 +1,37 @@
-import { Button as ChakraButtonComponent } from "@chakra-ui/react";
+import React from "react";
 
-interface ButtonProps
-  extends React.ComponentProps<typeof ChakraButtonComponent> {
-  //Add additional prop definitions here
+// Define a type for the possible "as" values
+type AsType = 'a' | 'button';
+
+// Create a union type for the allowed props based on "as" value
+type ElementProps = AsType extends 'a'
+  ? React.AnchorHTMLAttributes<HTMLAnchorElement>
+  : React.ButtonHTMLAttributes<HTMLButtonElement>;
+
+interface ButtonProps extends ElementProps {
+  as?: AsType;
+  href?: string;
 }
 
-const DefaultButton = (props: ButtonProps) => {
-  return (
-    <ChakraButtonComponent {...props}>{props.children}</ChakraButtonComponent>
-  );
+const DefaultButton = ({as = "button", children, className, ...props}: ButtonProps) => {
+  if( as === "a"){
+    const {...anchorProps} = props as React.AnchorHTMLAttributes<HTMLAnchorElement>;
+    return (
+      <a {...anchorProps} className={(className ? `${className}` : ``) +
+      ` inline-flex appearance-none items-center justify-center relative whitespace-nowrap align-middle outline outline-2 outline-transparent outline-offset-2 leading-tight font-semibold transition duration-200 h-10 min-w-[2.5rem] text-base px-4 py-3`}>
+        {children}
+      </a>
+    )
+  }
+  else{
+    const {...buttonProps} = props as React.ButtonHTMLAttributes<HTMLButtonElement>;
+    return (
+      <button {...buttonProps} className={(className ? `${className}` : ``) +
+      ` inline-flex appearance-none items-center justify-center relative whitespace-nowrap align-middle outline outline-2 outline-transparent outline-offset-2 leading-tight font-semibold transition duration-200 h-10 min-w-[2.5rem] text-base px-4 py-3`}>
+        {children}
+      </button>
+    )
+  }
 };
 
 export default DefaultButton;
