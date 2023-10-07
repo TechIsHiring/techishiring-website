30161fd7cc842104fe8b6ffe4996e78147356bc0
Wayne Jones <waynejones@nyu.edu>
2023-10-07 01:26:13 +0000

    Refactored TextBox component and removed ChakraUI components

--- src/components/atoms/textbox/textbox.tsx
+++ src/components/atoms/textbox/textbox.tsx
@@ -1,21 +1,13 @@
-import { Textarea as ChakraTextboxComponent } from "@chakra-ui/react";
 import React from "react";
 
-interface TextboxProps extends React.ComponentProps<typeof ChakraTextboxComponent> {
+interface TextboxProps extends React.DetailedHTMLProps<React.TextareaHTMLAttributes<HTMLTextAreaElement>, HTMLTextAreaElement> {
   //Add additional prop definitions here
 }
 
-type Textbox = TextboxProps;
-
-const TextBox = (props: Textbox) => {
+const TextBox = ({className, ...props}: TextboxProps) => {
   return (
-    <ChakraTextboxComponent
-      {...props}
-      className={`${props.className ? props.className : ""}`}
-      borderColor="blackAlpha.500"
-      focusBorderColor="blue.400"
-      _hover={{ }}
-    />
+    <textarea {...props} className={(className ? `${className}` : ``) +
+    ` w-full outline outline-2 outline-transparent outline-offset-2 relative appearance-none min-h-[5rem] leading-snug align-top text-base py-2 px-4 rounded-md border border-black/[.36] bg-inherit transition duration-200 focus:border-[#4299e1] focus:z-[1] focus:shadow-[0_0_0_1px_#4299e1]`} />
   );
 };
 
