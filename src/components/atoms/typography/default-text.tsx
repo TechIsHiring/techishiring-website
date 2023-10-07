b96195685c52ff7e293045f3e94ee27aa17fc7e7
Wayne Jones <waynejones@nyu.edu>
2023-10-07 20:13:48 +0000

    Refactored DefaultText component to allow for as attribute

--- src/components/atoms/typography/default-text.tsx
+++ src/components/atoms/typography/default-text.tsx
@@ -1,10 +1,25 @@
-interface DefaultTextProps {
-  className?: string;
-  children: React.ReactNode;
+import React from "react";
+
+// Define a type for the possible "as" values
+type AsType = 'span' | 'p';
+
+// Create a union type for the allowed props based on "as" value
+type ElementProps = AsType extends 'span'
+  ? React.HTMLAttributes<HTMLSpanElement>
+  : React.HTMLAttributes<HTMLParagraphElement>;
+
+interface DefaultTextProps extends ElementProps {
+  as?: AsType;
 }
 
-const DefaultText = ({className, children}: DefaultTextProps) => {
-  return <p className={className}>{children}</p>;
+const DefaultText = ({as = "p", className, children, ...props}: DefaultTextProps) => {
+  if(as === "span"){
+    return <span {...props} className={className}>{children}</span>;
+  }
+  else{
+    return <p {...props} className={className}>{children}</p>;
+  }
+  
 };
 
 export default DefaultText;
