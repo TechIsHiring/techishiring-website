6e6d6d78ebb3a8aaea243106ae9e71455342c49f
Wayne Jones <waynejones@nyu.edu>
2023-10-07 03:09:59 +0000

    Moving width out of textinput and textbox component

--- src/components/atoms/text-input/text-input.tsx
+++ src/components/atoms/text-input/text-input.tsx
@@ -7,7 +7,7 @@ interface InputProps extends React.DetailedHTMLProps<React.InputHTMLAttributes<H
 const TextInput = ({className, ...props}: InputProps) => {
   return (
     <input {...props} className={(className ? `${className}` : ``) +
-    ` w-full outline outline-2 outline-transparent outline-offset-2 relative appearance-none text-base px-4 h-10 rounded-md border border-black/[.36] bg-inherit transition duration-200 focus:border-[#4299e1] focus:z-[1] focus:shadow-[0_0_0_1px_#4299e1]`}  />    
+    ` outline outline-2 outline-transparent outline-offset-2 relative appearance-none text-base px-4 h-10 rounded-md border border-black/[.36] bg-inherit transition duration-200 focus:border-[#4299e1] focus:z-[1] focus:shadow-[0_0_0_1px_#4299e1]`}  />    
   );
 };
 
--- src/components/atoms/textbox/textbox.tsx
+++ src/components/atoms/textbox/textbox.tsx
@@ -7,7 +7,7 @@ interface TextboxProps extends React.DetailedHTMLProps<React.TextareaHTMLAttribu
 const TextBox = ({className, ...props}: TextboxProps) => {
   return (
     <textarea {...props} className={(className ? `${className}` : ``) +
-    ` w-full outline outline-2 outline-transparent outline-offset-2 relative appearance-none min-h-[5rem] leading-snug align-top text-base py-2 px-4 rounded-md border border-black/[.36] bg-inherit transition duration-200 focus:border-[#4299e1] focus:z-[1] focus:shadow-[0_0_0_1px_#4299e1]`} />
+    ` outline outline-2 outline-transparent outline-offset-2 relative appearance-none min-h-[5rem] leading-snug align-top text-base py-2 px-4 rounded-md border border-black/[.36] bg-inherit transition duration-200 focus:border-[#4299e1] focus:z-[1] focus:shadow-[0_0_0_1px_#4299e1]`} />
   );
 };
 
