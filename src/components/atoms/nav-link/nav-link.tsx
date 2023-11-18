118b8e6f5a0047f89e98983a8a87e690f13c7f35
Wayne Jones <2455598+Wayne-Jones@users.noreply.github.com>
2023-11-17 20:11:44 -0500

    Fixing NavLink sizing

--- src/components/atoms/nav-link/nav-link.tsx
+++ src/components/atoms/nav-link/nav-link.tsx
@@ -16,14 +16,13 @@ const NavLink = ({ url, text, activeLink, button, externalLink, footer = false }
         <DefaultButton
           className="text-white rounded-md bg-[#385898] hover:bg-[#314E89] active:bg-[#29487D] focus-visible:shadow-[0_0_0_3px_rgb(66,153,225,0.6)]"
         >
-          <DefaultText className="text-white" fontSize="lg" as="span">
+          <DefaultText className="text-white text-lg leading-[21.6px]" as="span">
             {text}
           </DefaultText>
         </DefaultButton>
       : 
         <DefaultText
-          className={`${activeLink ? "border-b-2 border-blue-500 text-blue-500" : ""}`}
-          fontSize="lg"
+          className={`${activeLink ? "border-b-2 border-blue-500 text-blue-500" : ""} text-lg`}
           as="span">
           {text}
         </DefaultText>
