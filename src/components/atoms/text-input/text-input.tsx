508c08249138fe8234ebb210545835437783f277
Wayne Jones <2455598+Wayne-Jones@users.noreply.github.com>
2023-11-17 19:05:21 -0500

    Adding isInvalid state to TextInput and TextBox

--- src/components/atoms/text-input/text-input.tsx
+++ src/components/atoms/text-input/text-input.tsx
@@ -1,13 +1,15 @@
 import React from "react";
+import { Input as ChakraInputComponent } from "@chakra-ui/react";
 
 interface InputProps extends React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> {
   //Add additional prop definitions here
+  isInvalid: boolean;
 }
 
-const TextInput = ({className, ...props}: InputProps) => {
+const TextInput = ({className, isInvalid, ...props}: InputProps) => {
   return (
-    <input {...props} className={(className ? `${className}` : ``) +
-    ` outline outline-2 outline-transparent outline-offset-2 relative appearance-none text-base px-4 h-10 rounded-md border border-black/[.36] bg-inherit transition duration-200 focus:border-[#4299e1] focus:z-[1] focus:shadow-[0_0_0_1px_#4299e1]`}  />    
+    <input {...props} className={(className ? `${className} ` : ``) + (isInvalid ? `border-[#E53E3E] shadow-[0_0_0_1px_#E53E3E] ` : `border-black/[.36] `) +
+    `outline outline-2 outline-transparent outline-offset-2 relative appearance-none text-base px-4 h-10 rounded-md border bg-inherit transition duration-200 focus:border-[#4299e1] focus:z-[1] focus:shadow-[0_0_0_1px_#4299e1]`}  />    
   );
 };
 
--- src/components/atoms/textbox/textbox.tsx
+++ src/components/atoms/textbox/textbox.tsx
@@ -2,11 +2,12 @@ import React from "react";
 
 interface TextboxProps extends React.DetailedHTMLProps<React.TextareaHTMLAttributes<HTMLTextAreaElement>, HTMLTextAreaElement> {
   //Add additional prop definitions here
+  isInvalid: boolean;
 }
 
-const TextBox = ({className, ...props}: TextboxProps) => {
+const TextBox = ({className, isInvalid, ...props}: TextboxProps) => {
   return (
-    <textarea {...props} className={(className ? `${className}` : ``) +
+    <textarea {...props} className={(className ? `${className} ` : ``) + (isInvalid ? `border-[#E53E3E] shadow-[0_0_0_1px_#E53E3E] ` : `border-black/[.36] `) +
     ` outline outline-2 outline-transparent outline-offset-2 relative appearance-none min-h-[5rem] leading-snug align-top text-base py-2 px-4 rounded-md border border-black/[.36] bg-inherit transition duration-200 focus:border-[#4299e1] focus:z-[1] focus:shadow-[0_0_0_1px_#4299e1]`} />
   );
 };
