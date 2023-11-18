2f9dae8e1b7bf7efc078c71f3a1119100d151b1a
Wayne Jones <2455598+Wayne-Jones@users.noreply.github.com>
2023-11-17 19:33:34 -0500

    Fixing bug with textbox border

--- src/components/atoms/textbox/textbox.tsx
+++ src/components/atoms/textbox/textbox.tsx
@@ -8,7 +8,7 @@ interface TextboxProps extends React.DetailedHTMLProps<React.TextareaHTMLAttribu
 const TextBox = ({className, isInvalid, ...props}: TextboxProps) => {
   return (
     <textarea {...props} className={(className ? `${className} ` : ``) + (isInvalid ? `border-[#E53E3E] shadow-[0_0_0_1px_#E53E3E] ` : `border-black/[.36] `) +
-    ` outline outline-2 outline-transparent outline-offset-2 relative appearance-none min-h-[5rem] leading-snug align-top text-base py-2 px-4 rounded-md border border-black/[.36] bg-inherit transition duration-200 focus:border-[#4299e1] focus:z-[1] focus:shadow-[0_0_0_1px_#4299e1]`} />
+    ` outline outline-2 outline-transparent outline-offset-2 relative appearance-none min-h-[5rem] leading-snug align-top text-base py-2 px-4 rounded-md border bg-inherit transition duration-200 focus:border-[#4299e1] focus:z-[1] focus:shadow-[0_0_0_1px_#4299e1]`} />
   );
 };
 
