747f5e265d174fa89145b91ac43b9c19425accfe
Wayne Jones <2455598+Wayne-Jones@users.noreply.github.com>
2023-11-17 19:49:10 -0500

    Styling fixes for TextInput and TextBox

--- src/components/atoms/text-input/text-input.tsx
+++ src/components/atoms/text-input/text-input.tsx
@@ -9,7 +9,7 @@ interface InputProps extends React.DetailedHTMLProps<React.InputHTMLAttributes<H
 const TextInput = ({className, isInvalid, ...props}: InputProps) => {
   return (
     <input {...props} className={(className ? `${className} ` : ``) + (isInvalid ? `border-[#E53E3E] shadow-[0_0_0_1px_#E53E3E] ` : `border-black/[.36] `) +
-    `outline outline-2 outline-transparent outline-offset-2 relative appearance-none text-base px-4 h-10 rounded-md border bg-inherit transition duration-200 focus:border-[#4299e1] focus:z-[1] focus:shadow-[0_0_0_1px_#4299e1]`}  />    
+    `outline outline-2 outline-transparent outline-offset-2 relative appearance-none text-base px-4 h-10 rounded-md border bg-inherit transition duration-200 focus:border-[#4299e1] focus:z-[1] focus:shadow-[0_0_0_1px_#4299e1] mt-2`}  />    
   );
 };
 
