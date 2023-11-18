fe965357062d6566e6beee86c00280253208795c
Wayne Jones <2455598+Wayne-Jones@users.noreply.github.com>
2023-11-18 00:55:23 -0500

    Refactoring Spinner component

--- src/components/atoms/spinner/spinner.tsx
+++ src/components/atoms/spinner/spinner.tsx
@@ -1,20 +1,26 @@
-import { Spinner as ChakraSpinnerComponent } from "@chakra-ui/react";
 import React from "react";
 
-interface SpinnerProps extends React.ComponentProps<typeof ChakraSpinnerComponent> {
+interface SpinnerProps extends React.DetailedHTMLProps<React.SVGProps<SVGSVGElement>, SVGSVGElement> {
   //Add additional prop definitions here
+  spinnerColor?: RGB | RGBA | HEX,
+  bgColor?: RGB | RGBA | HEX,
+  bgOpacity?: number
 }
 
-const Spinner = (props: SpinnerProps) => {
+type RGB = `rgb(${number}, ${number}, ${number})`;
+type RGBA = `rgba(${number}, ${number}, ${number}, ${number})`;
+type HEX = `#${string}`;
+
+const Spinner = ({ className = "", spinnerColor = "#3182ce", bgColor = "#E2E8F0", bgOpacity = .6, ...props }: SpinnerProps) => {
   return (
-    <ChakraSpinnerComponent
-      {...props}
-      thickness='4px'
-      speed='0.65s'
-      emptyColor='gray.200'
-      color='blue.500'
-      size='xl'
-    />
+    <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 38 38" stroke={spinnerColor} className={`${className} animate-spin`} {...props}>
+      <g fill="none" fill-rule="evenodd">
+        <g transform="translate(1 1)" stroke-width="2">
+          <circle stroke-opacity={bgOpacity} cx="18" cy="18" r="18" stroke={bgColor} />
+          <path d="M36 18c0-9.94-8.06-18-18-18" />
+        </g>
+      </g>
+    </svg>
   );
 };
 
